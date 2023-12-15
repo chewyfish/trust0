d294a6097bb37bd8c44b5312afc243fc29537eee
chewyfish <52176971+chewyfish@users.noreply.github.com>
2023-12-14 17:40:21 -0800

    TEST: [cli] Add unit tests for service manager module

--- crates/client/src/service/manager.rs
+++ crates/client/src/service/manager.rs
@@ -40,8 +40,31 @@ impl ProxyAddrs {
     pub fn get_gateway_port(&self) -> u16 {
         self.2
     }
-
 }
+
+/// Handles management of service proxy connections
+pub trait ServiceMgr : Send {
+
+    /// Active proxy service's ID for given proxy key
+    fn get_proxy_service_for_proxy_key(&self, proxy_key: &str) -> Option<u64>;
+
+    /// Proxy addresses for active service proxy
+    fn get_proxy_addrs_for_service(&self, service_id: u64) -> Option<ProxyAddrs>;
+
+    /// Active proxy visitor for given service
+    fn get_proxy_visitor_for_service(&self, service_id: u64) -> Option<&Arc<Mutex<dyn ClientServiceProxyVisitor>>>;
+
+    /// Clone proxy tasks sender
+    fn clone_proxy_tasks_sender(&self) -> Sender<ProxyExecutorEvent>;
+
+    /// Startup new proxy service to allow clients to connect/communicate to given service
+    fn startup(&mut self, service: &Service, proxy_addrs: &ProxyAddrs)
+        -> Result<ProxyAddrs, AppError>;
+
+    /// Shutdown all connected services, and respective proxy connections/listeners
+    fn shutdown(&mut self) -> Result<(), AppError>;
+}
+
 /// Manage service connections for client session.  Only one of these should be constructed.
 pub struct ClientServiceMgr {
     app_config: Arc<AppConfig>,
@@ -51,7 +74,8 @@ pub struct ClientServiceMgr {
     service_addrs: HashMap<u64, ProxyAddrs>,
     services_by_proxy_key: Arc<Mutex<HashMap<String, u64>>>,
     proxy_events_sender: Sender<ProxyEvent>,
-    proxy_tasks_sender: Sender<ProxyExecutorEvent>
+    proxy_tasks_sender: Sender<ProxyExecutorEvent>,
+    testing_mode: bool
 }
 
 impl ClientServiceMgr {
@@ -70,27 +94,38 @@ impl ClientServiceMgr {
             service_addrs: HashMap::new(),
             services_by_proxy_key: Arc::new(Mutex::new(HashMap::new())),
             proxy_events_sender,
-            proxy_tasks_sender
+            proxy_tasks_sender,
+            testing_mode: false
         }
     }
 
+    /// Listen and process any proxy events (blocking)
     pub fn poll_proxy_events(service_mgr: Arc<Mutex<dyn ServiceMgr>>, proxy_events_receiver: Receiver<ProxyEvent>)
         -> Result<(), AppError> {
-        'EVENTS:
+
         loop {
-            let proxy_event = proxy_events_receiver.recv().map_err(|err|
-                AppError::GenWithMsgAndErr("Error receiving proxy event".to_string(), Box::new(err)))?;
+            Self::process_next_proxy_event(&service_mgr, &proxy_events_receiver)?;
+        }
+    }
 
-            if let ProxyEvent::Closed(proxy_key) = proxy_event {
-                let service_id = service_mgr.lock().unwrap().get_proxy_service_for_proxy_key(&proxy_key).unwrap_or(u64::MAX);
+    /// Process next queued proxy event (blocking). Returns whether processing occurred
+    fn process_next_proxy_event(service_mgr: &Arc<Mutex<dyn ServiceMgr>>, proxy_events_receiver: &Receiver<ProxyEvent>)
+        -> Result<bool, AppError> {
 
-                if let Some(proxy_visitor) = service_mgr.lock().unwrap().get_proxy_visitor_for_service(service_id) {
-                    if proxy_visitor.lock().unwrap().remove_proxy_for_key(&proxy_key) {
-                        continue 'EVENTS;
-                    }
+        let proxy_event = proxy_events_receiver.recv().map_err(|err|
+            AppError::GenWithMsgAndErr("Error receiving proxy event".to_string(), Box::new(err)))?;
+
+        if let ProxyEvent::Closed(proxy_key) = proxy_event {
+            let service_id = service_mgr.lock().unwrap().get_proxy_service_for_proxy_key(&proxy_key).unwrap_or(u64::MAX);
+
+            if let Some(proxy_visitor) = service_mgr.lock().unwrap().get_proxy_visitor_for_service(service_id) {
+                if proxy_visitor.lock().unwrap().remove_proxy_for_key(&proxy_key) {
+                    return Ok(true);
                 }
             }
         }
+
+        Ok(false)
     }
 }
 
@@ -112,9 +147,7 @@ impl ServiceMgr for ClientServiceMgr {
         self.proxy_tasks_sender.clone()
     }
 
-    fn startup(&mut self,
-               service: &Service,
-               proxy_addrs: &ProxyAddrs)
+    fn startup(&mut self, service: &Service, proxy_addrs: &ProxyAddrs)
         -> Result<ProxyAddrs, AppError> {
 
         // Service proxy already started
@@ -127,7 +160,6 @@ impl ServiceMgr for ClientServiceMgr {
         // - - - - - - - - - - - - - - -
         let service_proxy: Arc<Mutex<dyn ClientServiceProxy>>;
         let service_proxy_visitor: Arc<Mutex<dyn ClientServiceProxyVisitor>>;
-        let service_proxy_thread: JoinHandle<Result<(), AppError>>;
 
         match service.transport {
 
@@ -150,10 +182,13 @@ impl ServiceMgr for ClientServiceMgr {
 
                 service_proxy_visitor = tcp_proxy_visitor;
 
-                let service_proxy_closure = service_proxy.clone();
-                service_proxy_thread = thread::spawn(move || {
-                    service_proxy_closure.lock().unwrap().startup()
-                });
+                if ! self.testing_mode {
+                    let service_proxy_closure = service_proxy.clone();
+                    let service_proxy_thread = thread::spawn(move || {
+                        service_proxy_closure.lock().unwrap().startup()
+                    });
+                    self.service_proxy_threads.insert(service.service_id, service_proxy_thread);
+                }
             }
 
             // Starts up UDP service proxy
@@ -179,28 +214,31 @@ impl ServiceMgr for ClientServiceMgr {
 
                 service_proxy_visitor = udp_proxy_visitor;
 
-                let service_proxy_closure = service_proxy.clone();
-                service_proxy_thread = thread::spawn(move || {
-                    service_proxy_closure.lock().unwrap().startup()
-                });
+                if ! self.testing_mode {
+                    let service_proxy_closure = service_proxy.clone();
+                    let service_proxy_thread = thread::spawn(move || {
+                        service_proxy_closure.lock().unwrap().startup()
+                    });
+                    self.service_proxy_threads.insert(service.service_id, service_proxy_thread);
+                }
             }
         }
 
         self.service_addrs.insert(service.service_id, proxy_addrs.clone());
         self.service_proxies.insert(service.service_id, service_proxy);
         self.service_proxy_visitors.insert(service.service_id, service_proxy_visitor);
-        self.service_proxy_threads.insert(service.service_id, service_proxy_thread);
 
         Ok(proxy_addrs.clone())
     }
 
     fn shutdown(&mut self) -> Result<(), AppError> {
+
         let mut errors: Vec<String> = vec![];
 
         self.service_proxy_visitors.iter().for_each(|(proxy_service_id, proxy_visitor)| {
             let mut proxy_visitor = proxy_visitor.lock().unwrap();
 
-            proxy_visitor.deref_mut().set_shutdown_requested(true);
+            proxy_visitor.deref_mut().set_shutdown_requested();
 
             if let Err(err) = proxy_visitor.deref_mut().shutdown_connections(self.clone_proxy_tasks_sender()) {
                 errors.push(format!("Failed shutting down service proxy: svc_id={}, err={:?}", proxy_service_id, err));
@@ -217,35 +255,15 @@ impl ServiceMgr for ClientServiceMgr {
     }
 }
 
-pub trait ServiceMgr : Send {
-
-    /// Active proxy service's ID for given proxy key
-    fn get_proxy_service_for_proxy_key(&self, proxy_key: &str) -> Option<u64>;
-
-    /// Proxy addresses for active service proxy
-    fn get_proxy_addrs_for_service(&self, service_id: u64) -> Option<ProxyAddrs>;
-
-    /// Active proxy visitor for given service
-    fn get_proxy_visitor_for_service(&self, service_id: u64) -> Option<&Arc<Mutex<dyn ClientServiceProxyVisitor>>>;
-
-        /// Clone proxy tasks sender
-    fn clone_proxy_tasks_sender(&self) -> Sender<ProxyExecutorEvent>;
-
-    /// Startup new proxy service to allow clients to connect/communicate to given service
-    fn startup(&mut self,
-               service: &Service,
-               proxy_addrs: &ProxyAddrs)
-               -> Result<ProxyAddrs, AppError>;
-
-    /// Shutdown all connected services, and respective proxy connections/listeners
-    fn shutdown(&mut self) -> Result<(), AppError>;
-}
-
 /// Unit tests
 #[cfg(test)]
 pub mod tests {
 
-    use mockall::mock;
+    use std::net::SocketAddr;
+    use std::str::FromStr;
+    use mockall::{mock, predicate};
+    use crate::config;
+    use crate::service::proxy::proxy::tests::MockCliSvcProxyVisitor;
     use super::*;
 
     // mocks
@@ -262,4 +280,171 @@ pub mod tests {
             fn shutdown(&mut self) -> Result<(), AppError>;
         }
     }
+
+    // tests
+    // =====
+
+    #[test]
+    fn clisvcmgr_process_next_proxy_event_when_ignorable_evt() {
+
+        let app_config = Arc::new(config::tests::create_app_config(None).unwrap());
+        let events_channel = mpsc::channel();
+        let proxy_key="proxykey1".to_string();
+        let proxy_svc_id = 123;
+
+        let mut proxy_visitor = MockCliSvcProxyVisitor::new();
+        proxy_visitor.expect_remove_proxy_for_key().with(predicate::eq(proxy_key.clone())).never();
+
+        let mut service_mgr = ClientServiceMgr::new(app_config, mpsc::channel().0, events_channel.0.clone());
+        service_mgr.testing_mode = true;
+        service_mgr.services_by_proxy_key.lock().unwrap().insert(proxy_key.clone(), proxy_svc_id);
+        service_mgr.service_proxy_visitors.insert(proxy_svc_id, Arc::new(Mutex::new(proxy_visitor)));
+        let service_mgr: Arc<Mutex<dyn ServiceMgr>> = Arc::new(Mutex::new(service_mgr));
+
+        let msg = ProxyEvent::Message(proxy_key.clone(), SocketAddr::from_str("127.0.0.1:3000").unwrap(), "data".as_bytes().to_vec());
+        events_channel.0.send(msg).unwrap();
+
+        match ClientServiceMgr::process_next_proxy_event(&service_mgr, &events_channel.1) {
+            Ok(processed) => {
+                assert_eq!(processed, false);
+            }
+            Err(err) => panic!("Unexpected result: err={:?}", &err)
+        }
+    }
+
+    #[test]
+    fn clisvcmgr_process_next_proxy_event_when_closed_evt() {
+
+        let app_config = Arc::new(config::tests::create_app_config(None).unwrap());
+        let events_channel = mpsc::channel();
+        let proxy_key="proxykey1".to_string();
+        let proxy_svc_id = 123;
+
+        let mut proxy_visitor = MockCliSvcProxyVisitor::new();
+        proxy_visitor.expect_remove_proxy_for_key().with(predicate::eq(proxy_key.clone())).times(1).return_once(|_| { true });
+
+        let mut service_mgr = ClientServiceMgr::new(app_config, mpsc::channel().0, events_channel.0.clone());
+        service_mgr.testing_mode = true;
+        service_mgr.services_by_proxy_key.lock().unwrap().insert(proxy_key.clone(), proxy_svc_id);
+        service_mgr.service_proxy_visitors.insert(proxy_svc_id, Arc::new(Mutex::new(proxy_visitor)));
+        let service_mgr: Arc<Mutex<dyn ServiceMgr>> = Arc::new(Mutex::new(service_mgr));
+
+        let msg = ProxyEvent::Closed(proxy_key.clone());
+        events_channel.0.send(msg).unwrap();
+
+        match ClientServiceMgr::process_next_proxy_event(&service_mgr, &events_channel.1) {
+            Ok(processed) => {
+                assert_eq!(processed, true);
+            }
+            Err(err) => panic!("Unexpected result: err={:?}", &err)
+        }
+    }
+
+    #[test]
+    fn clisvcmgr_startup_when_already_started() {
+
+        let service = Service { service_id: 200, name: "Service200".to_string(), transport:Transport::TCP, host: "localhost".to_string(), port: 8200 };
+        let app_config = Arc::new(config::tests::create_app_config(None).unwrap());
+        let proxy_addrs = ProxyAddrs(3000, "gwhost1".to_string(), 8000);
+
+        let mut service_mgr = ClientServiceMgr::new(app_config, mpsc::channel().0, mpsc::channel().0);
+        service_mgr.testing_mode = true;
+        service_mgr.service_addrs.insert(service.service_id, proxy_addrs.clone());
+
+        let orig_svc_addrs_len = service_mgr.service_addrs.len();
+        let orig_svc_proxies_len = service_mgr.service_proxies.len();
+        let orig_svc_proxy_visitors_len = service_mgr.service_proxy_visitors.len();
+
+        match service_mgr.startup(&service, &proxy_addrs) {
+            Ok(result_proxy_addrs) => {
+                assert_eq!(result_proxy_addrs, proxy_addrs);
+            }
+            Err(err) => {
+                panic!("Unexpected startup result: err={:?}", &err);
+            }
+        }
+
+        assert_eq!(service_mgr.service_addrs.len(), orig_svc_addrs_len);
+        assert_eq!(service_mgr.service_proxies.len(), orig_svc_proxies_len);
+        assert_eq!(service_mgr.service_proxy_visitors.len(), orig_svc_proxy_visitors_len);
+    }
+
+    #[test]
+    fn clisvcmgr_start_when_tcp_service() {
+
+        let service = Service { service_id: 200, name: "Service200".to_string(), transport:Transport::TCP, host: "localhost".to_string(), port: 8200 };
+        let app_config = Arc::new(config::tests::create_app_config(None).unwrap());
+        let proxy_addrs = ProxyAddrs(3000, "gwhost1".to_string(), 8000);
+
+        let mut service_mgr = ClientServiceMgr::new(app_config, mpsc::channel().0, mpsc::channel().0);
+        service_mgr.testing_mode = true;
+
+        let orig_svc_addrs_len = service_mgr.service_addrs.len();
+        let orig_svc_proxies_len = service_mgr.service_proxies.len();
+        let orig_svc_proxy_visitors_len = service_mgr.service_proxy_visitors.len();
+
+        match service_mgr.startup(&service, &proxy_addrs) {
+            Ok(result_proxy_addrs) => {
+                assert_eq!(result_proxy_addrs, proxy_addrs);
+            }
+            Err(err) => {
+                panic!("Unexpected startup result: err={:?}", &err);
+            }
+        }
+
+        assert_eq!(service_mgr.service_addrs.len(), orig_svc_addrs_len + 1);
+        assert_eq!(service_mgr.service_proxies.len(), orig_svc_proxies_len + 1);
+        assert_eq!(service_mgr.service_proxy_visitors.len(), orig_svc_proxy_visitors_len + 1);
+    }
+
+    #[test]
+    fn clisvcmgr_start_when_udp_service() {
+
+        let service = Service { service_id: 200, name: "Service200".to_string(), transport:Transport::UDP, host: "localhost".to_string(), port: 8200 };
+        let app_config = Arc::new(config::tests::create_app_config(None).unwrap());
+        let proxy_addrs = ProxyAddrs(3000, "gwhost1".to_string(), 8000);
+
+        let mut service_mgr = ClientServiceMgr::new(app_config, mpsc::channel().0, mpsc::channel().0);
+        service_mgr.testing_mode = true;
+
+        let orig_svc_addrs_len = service_mgr.service_addrs.len();
+        let orig_svc_proxies_len = service_mgr.service_proxies.len();
+        let orig_svc_proxy_visitors_len = service_mgr.service_proxy_visitors.len();
+
+        match service_mgr.startup(&service, &proxy_addrs) {
+            Ok(result_proxy_addrs) => {
+                assert_eq!(result_proxy_addrs, proxy_addrs);
+            }
+            Err(err) => {
+                panic!("Unexpected startup result: err={:?}", &err);
+            }
+        }
+
+        assert_eq!(service_mgr.service_addrs.len(), orig_svc_addrs_len + 1);
+        assert_eq!(service_mgr.service_proxies.len(), orig_svc_proxies_len + 1);
+        assert_eq!(service_mgr.service_proxy_visitors.len(), orig_svc_proxy_visitors_len + 1);
+    }
+
+    #[test]
+    fn clisvcmgr_shutdown_when_2_service_proxies() {
+
+        let app_config = Arc::new(config::tests::create_app_config(None).unwrap());
+
+        let mut proxy_visitor200 = MockCliSvcProxyVisitor::new();
+        proxy_visitor200.expect_set_shutdown_requested().times(1).return_once(|| {()});
+        proxy_visitor200.expect_shutdown_connections().times(1).return_once(|_| { Ok(()) });
+        let mut proxy_visitor201 = MockCliSvcProxyVisitor::new();
+        proxy_visitor201.expect_set_shutdown_requested().times(1).return_once(|| {()});
+        proxy_visitor201.expect_shutdown_connections().times(1).return_once(|_| { Ok(()) });
+
+        let mut service_mgr = ClientServiceMgr::new(app_config, mpsc::channel().0, mpsc::channel().0);
+        service_mgr.testing_mode = true;
+        service_mgr.service_proxy_visitors.insert(200, Arc::new(Mutex::new(proxy_visitor200)));
+        service_mgr.service_proxy_visitors.insert(201, Arc::new(Mutex::new(proxy_visitor201)));
+
+        if let Err(err) = service_mgr.shutdown() {
+            panic!("Unexpected result: err={:?}", &err);
+        }
+    }
+
 }
