fe39e83305e971fe7cd76c11378b4359c7862196
chewyfish <52176971+chewyfish@users.noreply.github.com>
2024-01-14 17:46:53 -0800

    TEST: [cmn] Add unit tests for config module

--- crates/common/src/config.rs
+++ crates/common/src/config.rs
@@ -7,3 +7,19 @@ pub fn exit_on_usage_error(error: ErrorKind, command: &mut Command, message: &st
     error!("{:?}", command.render_help());
     panic!("{}: {}", error, message);
 }
+
+/// Unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn config_exit_on_usage_error() {
+        exit_on_usage_error(
+            ErrorKind::DisplayHelp,
+            &mut Command::new("My Command"),
+            "msg1",
+        );
+    }
+}
