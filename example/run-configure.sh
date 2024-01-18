6d6d3ac16cce653b3c41bb658cfbb349adc16d8d
chewyfish <52176971+chewyfish@users.noreply.github.com>
2024-01-17 22:10:58 -0800

    BUG: [example] Corrected order of example setup steps - Required config file before it was generated - Moved pre-requisite command checking to configure script - Added check for bash 4+ (using 4+ features in admin tool)

--- example/run-configure.sh
+++ example/run-configure.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 EXAMPLE_DIR=$(dirname "$0")
 EXAMPLE_BUILD_DIR="${EXAMPLE_DIR}/target"
 EXAMPLE_CONFIG_FILE="${EXAMPLE_BUILD_DIR}/example.conf"
+
 DATASOURCE_INMEMDB_ACCESS_M4_FILE="${EXAMPLE_DIR}/example-db-access.json.m4"
 DATASOURCE_INMEMDB_ACCESS_FILE="${EXAMPLE_BUILD_DIR}/example-db-access.json"
 DATASOURCE_INMEMDB_SERVICE_M4_FILE="${EXAMPLE_DIR}/example-db-service.json.m4"
@@ -13,7 +14,41 @@ DATASOURCE_INMEMDB_ROLE_M4_FILE="${EXAMPLE_DIR}/example-db-role.json.m4"
 DATASOURCE_INMEMDB_ROLE_FILE="${EXAMPLE_BUILD_DIR}/example-db-role.json"
 DATASOURCE_INMEMDB_USER_M4_FILE="${EXAMPLE_DIR}/example-db-user.json.m4"
 DATASOURCE_INMEMDB_USER_FILE="${EXAMPLE_BUILD_DIR}/example-db-user.json"
-M4_CMD=m4
+
+CARGO_CMD="cargo"
+OPENSSL_CMD="openssl"
+M4_CMD="m4"
+TMUX_CMD="tmux"
+NCAT_CMD="ncat"
+
+# Check pre-requisites
+
+PREREQ_MISSING=0
+
+if ((BASH_VERSINFO[0] < 4)); then
+  echo "Bash version 4 or higher required"
+  PREREQ_MISSING=1
+fi
+
+function check_command_exists() {
+  local command="$1"
+  if ! which "${command}" 2> /dev/null > /dev/null; then
+    echo "Command '${command}' missing, please install"
+    PREREQ_MISSING=1
+  fi
+}
+
+check_command_exists "${CARGO_CMD}"
+check_command_exists "${OPENSSL_CMD}"
+check_command_exists "${M4_CMD}"
+check_command_exists "${TMUX_CMD}"
+check_command_exists "${NCAT_CMD}"
+
+if [ "${PREREQ_MISSING}" == "1" ]; then
+  exit 1
+fi
+
+# Check if configuration is needed
 
 RECONFIGURE=n
 if [ -f "${EXAMPLE_CONFIG_FILE}" ]; then
@@ -26,6 +61,8 @@ else
 fi
 
 read -p "If example requires secondary authentication credentials, please use \"user1\", \"pass1\""
+
+# Create core application configuration files
 
 if [ "$RECONFIGURE" == 'y' ]; then
   mkdir -p "${EXAMPLE_BUILD_DIR}"
