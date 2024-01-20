4f7ed9e815b1160ceb7f923a9c22683798cc1a29
chewyfish <52176971+chewyfish@users.noreply.github.com>
2024-01-19 20:29:18 -0800

    TASK: [example] Surround command paths with quotes

--- example/run-chat-tcp-example.sh
+++ example/run-chat-tcp-example.sh
@@ -26,19 +26,19 @@ fi
 
 source "${EXAMPLE_DIR}"/target/example.conf
 
-PS1='$ ' ${TMUX_CMD} new-session -s trust0-chat-tcp-example \; \
+PS1='$ ' "${TMUX_CMD}" new-session -s trust0-chat-tcp-example \; \
   set -g mouse on \; \
-  send-keys '(clear && read -p "Step 1: Hit <Enter> to run chat server (or prior to executing chat client)" && '"${NCAT_CMD}"' -v -k -l -p '"${CHAT_SERVICE__PORT}"' --chat)' C-m \; \
+  send-keys '(clear && read -p "Step 1: Hit <Enter> to run chat server (or prior to executing chat client)" && "'"${NCAT_CMD}"'" -v -k -l -p '"${CHAT_SERVICE__PORT}"' --chat)' C-m \; \
   split-window -v -l 83% \; \
-  send-keys '(clear && read -p "Step 2: Hit <Enter> to run trust0 gateway" && '"${GMAKE_CMD}"' run-trust0-gateway-nodeps EXECBIN_EXTRA_ARGS="'"${GATEWAY_BIN_ARGS}"'")' C-m \; \
+  send-keys '(clear && read -p "Step 2: Hit <Enter> to run trust0 gateway" && "'"${GMAKE_CMD}"'" run-trust0-gateway-nodeps EXECBIN_EXTRA_ARGS="'"${GATEWAY_BIN_ARGS}"'")' C-m \; \
   split-window -v -l 67% \; \
-  send-keys '(clear && echo "Step 3.1: Hit <Enter> to run trust0 client (after gateway is up)" && read -p "Step 3.2: Enter \"start -s chat-tcp -p '"${CHAT_PROXY__PORT}"'\" (to start service proxy)" && '"${GMAKE_CMD}"' run-trust0-client-nodeps EXECBIN_EXTRA_ARGS="'"${CLIENT_BIN_ARGS}"'")' C-m \; \
+  send-keys '(clear && echo "Step 3.1: Hit <Enter> to run trust0 client (after gateway is up)" && read -p "Step 3.2: Enter \"start -s chat-tcp -p '"${CHAT_PROXY__PORT}"'\" (to start service proxy)" && "'"${GMAKE_CMD}"'" run-trust0-client-nodeps EXECBIN_EXTRA_ARGS="'"${CLIENT_BIN_ARGS}"'")' C-m \; \
   split-window -v -l 50% \; \
-  send-keys '(clear && read -p "Step 4: Hit <Enter> to run 1st chat client (after service proxy has started)" && '"${NCAT_CMD}"' -v localhost '"${CHAT_PROXY__PORT}"')' C-m \; \
+  send-keys '(clear && read -p "Step 4: Hit <Enter> to run 1st chat client (after service proxy has started)" && "'"${NCAT_CMD}"'" -v localhost '"${CHAT_PROXY__PORT}"')' C-m \; \
   split-window -h -l 50% \; \
-  send-keys '(clear && echo "Step 5.1: Hit <Enter> to run 2nd chat client (after service proxy has started)" && read -p "Step 5.2: Enter chat messages between chat clients" && '"${NCAT_CMD}"' -v localhost '"${CHAT_PROXY__PORT}"')' C-m \; \
+  send-keys '(clear && echo "Step 5.1: Hit <Enter> to run 2nd chat client (after service proxy has started)" && read -p "Step 5.2: Enter chat messages between chat clients" && "'"${NCAT_CMD}"'" -v localhost '"${CHAT_PROXY__PORT}"')' C-m \; \
   select-pane -t 0 \; \
   split-window -h -l 25% \; \
-  send-keys '(clear && read -p "Step Last: Hit <Enter> to shutdown example" && ('"${TMUX_CMD}"' kill-session -t trust0-chat-tcp-example))' C-m \; \
+  send-keys '(clear && read -p "Step Last: Hit <Enter> to shutdown example" && ("'"${TMUX_CMD}"'" kill-session -t trust0-chat-tcp-example))' C-m \; \
   select-pane -t 0 \;
 
--- example/run-revoke-cert-example.sh
+++ example/run-revoke-cert-example.sh
@@ -30,18 +30,18 @@ fi
 
 source "${EXAMPLE_DIR}/target/example.conf"
 
-PS1='$ ' ${TMUX_CMD} new-session -s trust0-echo-udp-example \; \
+PS1='$ ' "${TMUX_CMD}" new-session -s trust0-echo-udp-example \; \
   set -g mouse on \; \
-  send-keys '(clear && read -p "Step 1: Hit <Enter> to run trust0 gateway" && '"${GMAKE_CMD}"' run-trust0-gateway-nodeps EXECBIN_EXTRA_ARGS="'"${GATEWAY_BIN_ARGS}"'")' C-m \; \
+  send-keys '(clear && read -p "Step 1: Hit <Enter> to run trust0 gateway" && "'"${GMAKE_CMD}"'" run-trust0-gateway-nodeps EXECBIN_EXTRA_ARGS="'"${GATEWAY_BIN_ARGS}"'")' C-m \; \
   split-window -v -l 65% \; \
-  send-keys '(clear && echo "Step 2.1: Hit <Enter> to run trust0 client (after gateway is up)" && read -p "Step 2.2: Enter \"start -s echo-udp -p '"${ECHO_PROXY__PORT}"'\" (to start service proxy)" && '"${GMAKE_CMD}"' run-trust0-client-nodeps EXECBIN_EXTRA_ARGS="'"${CLIENT_BIN_ARGS}"'")' C-m \; \
+  send-keys '(clear && echo "Step 2.1: Hit <Enter> to run trust0 client (after gateway is up)" && read -p "Step 2.2: Enter \"start -s echo-udp -p '"${ECHO_PROXY__PORT}"'\" (to start service proxy)" && "'"${GMAKE_CMD}"'" run-trust0-client-nodeps EXECBIN_EXTRA_ARGS="'"${CLIENT_BIN_ARGS}"'")' C-m \; \
   split-window -v -l 43% \; \
-  send-keys '(clear && read -p "Step 3: Hit <Enter> to run echo server" && '"${NCAT_CMD}"' -v -u -k -l -p '"${ECHO_SERVICE__PORT}"' --exec '"${CAT_CMD}"')' C-m \; \
+  send-keys '(clear && read -p "Step 3: Hit <Enter> to run echo server" && "'"${NCAT_CMD}"'" -v -u -k -l -p '"${ECHO_SERVICE__PORT}"' --exec "'"${CAT_CMD}"'")' C-m \; \
   split-window -h -l 74% \; \
-  send-keys '(clear && read -p "Step 4: Hit <Enter> to run echo client (after service proxy has started)" && '"${NCAT_CMD}"' -v -u localhost '"${ECHO_PROXY__PORT}"')' C-m \; \
+  send-keys '(clear && read -p "Step 4: Hit <Enter> to run echo client (after service proxy has started)" && "'"${NCAT_CMD}"'" -v -u localhost '"${ECHO_PROXY__PORT}"')' C-m \; \
   split-window -h -l 62% \; \
-  send-keys '(clear && echo "Step 5: Hit <Enter> to run echo client (after service proxy has started)" && read -p "(Will delay 35s so that CRL can be reloaded)" && cp '"${CLIENT_REVOKED_CRLFILE}"' '"${GATEWAY_CONFIGURED_CRLFILE}"' && sleep 35 && '"${NCAT_CMD}"' -v -u localhost '"${ECHO_PROXY__PORT}"')' C-m \; \
+  send-keys '(clear && echo "Step 5: Hit <Enter> to run echo client (after service proxy has started)" && read -p "(Will delay 35s so that CRL can be reloaded)" && cp '"${CLIENT_REVOKED_CRLFILE}"' '"${GATEWAY_CONFIGURED_CRLFILE}"' && sleep 35 && "'"${NCAT_CMD}"'" -v -u localhost '"${ECHO_PROXY__PORT}"')' C-m \; \
   split-window -h -l 38% \; \
-  send-keys '(clear && read -p "Step Last: Hit <Enter> to shutdown example" && ('"${TMUX_CMD}"' kill-session -t trust0-echo-udp-example))' C-m \; \
+  send-keys '(clear && read -p "Step Last: Hit <Enter> to shutdown example" && ("'"${TMUX_CMD}"'" kill-session -t trust0-echo-udp-example))' C-m \; \
   select-pane -t 0 \;
 
